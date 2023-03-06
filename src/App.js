43ebd9a31a079788c672954892224bd37fd09667
Angelique De Castro <angeliqueajdecastro@gmail.com>
2023-03-06 13:57:42 -0500

    cleanup and update og description

--- src/App.js
+++ src/App.js
@@ -9,13 +9,13 @@ const containerClass = css`
 const App = () => (
   <>
     <link rel="canonical" href="index.css"/>
-    <meta name="description" content="FIREWALL is a pop-up computer kiosk that simultaneously SURFs the Internet locally and in China using a customized Google Chrome extension."/>
+    <meta name="description" content="FIREWALL juxtaposes Google and Baidu image searches and allows users to vote if images are censored" />
     <meta property="og:title" content="Firewall Cafe"/>
     <meta property="og:type" content="article"/>
     <meta property="og:url" content="https://firewallcafe.com"/>
-    <meta property="og:description" content="FIREWALL is a pop-up computer kiosk that simultaneously SURFs the Internet locally and in China using a customized Google Chrome extension."/>
-    {/* <meta property="og:image" content="{{seo.image}}"/> */}
-    <meta name="twitter:card" content="FIREWALL is a pop-up computer kiosk that simultaneously SURFs the Internet locally and in China using a customized Google Chrome extension."/>
+    <meta property="og:description" content="FIREWALL juxtaposes Google and Baidu image searches and allows users to vote if images are censored" />
+    <meta property="og:image" content="%PUBLIC_URL%/favicon.ico" />
+    <meta name="twitter:card" content="FIREWALL juxtaposes Google and Baidu image searches and allows users to vote if images are censored" />
     <div css={containerClass}>
       <h1>Firewall 2.0</h1>
       <Layout />
