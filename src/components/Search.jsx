f3b621c0a4b353c76684475830718c6f9e2523a2
Gabriel Williams <ummonai@gmail.com>
2024-10-24 22:17:44 -0700

    Search components and font. placeholder carousel

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -3,13 +3,16 @@ import React from 'react';
 import SearchInput from './SearchInput';
 import SearchCompare from './SearchCompare';
 import FeatureCards from './FeatureCards';
+import Archive from '../assets/icons/Archive_grayscale.png';
+import TimelineIcon from '../assets/icons/Timeline.png';
+import Commentary from '../assets/icons/expert-commentary_grayscale.png';
 
 const features = [
     {
       title: "Archive",
       chineseTitle: { text: "档案", color: "text-black border-black" },
       description: "Explore what other users have searched and vote on results.",
-      iconSrc: "https://cdn.builder.io/api/v1/image/assets/TEMP/43fd09d2605185335d23773f2561fcb2ded3adbfd5e94e9cc41d1b5c6f67684e?placeholderIfAbsent=true&apiKey=d56f8d62d9074d509de3faeb2651bd99",
+      iconSrc: Archive,
       bgColor: "bg-red-600",
       textColor: "text-white",
       borderColor: "border-red-600"
@@ -18,7 +21,7 @@ const features = [
       title: "Timeline",
       chineseTitle: { text: "时间线", color: "text-red-600 border-red-600" },
       description: "Why did all this happen?",
-      iconSrc: "https://cdn.builder.io/api/v1/image/assets/TEMP/c5c35978f81aa12a10a5251a03d41c24a605bb1871811e8cce8e99605780ce75?placeholderIfAbsent=true&apiKey=d56f8d62d9074d509de3faeb2651bd99",
+      iconSrc: TimelineIcon,
       bgColor: "bg-rose-100",
       textColor: "text-black",
       borderColor: "border-red-600"
@@ -27,7 +30,7 @@ const features = [
       title: "Expert commentary",
       chineseTitle: { text: "专家点评", color: "text-red-600 border-red-600" },
       description: "Read and listen to in-depth commentary from experts in the east and west.",
-      iconSrc: "https://cdn.builder.io/api/v1/image/assets/TEMP/93109040a8c5bb26e9936f7a12c249e8b5ba72d7f40fb61cdb12de89b360914d?placeholderIfAbsent=true&apiKey=d56f8d62d9074d509de3faeb2651bd99",
+      iconSrc: Commentary,
       bgColor: "bg-white",
       textColor: "text-black",
       borderColor: "border-red-600"
--- src/components/VotingSection.jsx
+++ src/components/VotingSection.jsx
@@ -1,4 +1,13 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+
+import Archive from '../assets/icons/Archive_grayscale.png';
+import Question from '../assets/icons/question.svg';
+import ThumbUp from '../assets/icons/thumb_up.svg';
+import ThumbDown from '../assets/icons/thumb_down.svg';
+import VisibilityOff from '../assets/icons/visibility_off.svg';
+import Visibility from '../assets/icons/visibility.svg';
+import LostInTranslation from '../assets/icons/lost_in_translation.svg';
 
 function VotingSection() {
   return (
@@ -8,20 +17,34 @@ function VotingSection() {
           <div className="flex gap-2.5 items-start w-full text-2xl text-black min-h-[61px] max-md:max-w-full">
             <div className="flex flex-wrap flex-1 shrink gap-2.5 items-center w-full basis-0 min-w-[240px] max-md:max-w-full">
               <div className="self-stretch my-auto max-md:max-w-full">What do you think about these search results?</div>
-              <img loading="lazy" src="https://cdn.builder.io/api/v1/image/assets/TEMP/a823733f765b51f85e9cddedb4d2f5875d317f037ccf314971e7a52b0ed5ba7a?placeholderIfAbsent=true&apiKey=d56f8d62d9074d509de3faeb2651bd99" className="object-contain shrink-0 self-stretch my-auto w-6 aspect-square" alt="" />
+              <img src={Question} className="object-contain shrink-0 self-stretch my-auto w-6 aspect-square" alt="" />
             </div>
           </div>
           <div className="flex flex-wrap gap-4 items-start mt-8 w-full rounded-md max-md:max-w-full">
-            {['Censored', 'Not censored', 'Lost in translation'].map((option, index) => (
-              <div key={index} className="flex flex-col grow shrink justify-between p-3 bg-white rounded border border-solid border-neutral-300 min-h-[124px] min-w-[190px] w-[152px]">
-                <div className="flex gap-2.5 items-center w-full h-9">
-                  <img loading="lazy" src={`http://b.io/ext_${16 + index}-`} className="object-contain self-stretch my-auto w-12 aspect-square" alt="" />
-                </div>
-                <div className="flex-1 shrink gap-2 self-stretch mt-10 w-full text-xl font-semibold leading-tight text-black">
-                  {option}
-                </div>
+            <div className="flex flex-col grow shrink justify-between items-center p-3 rounded border border-solid border-neutral-300 min-h-[124px]">
+              <div className="flex gap-2.5 items-center w-full h-9">
+                <img src={VisibilityOff} className="object-contain self-stretch my-auto w-12 aspect-square" alt="" />
               </div>
-            ))}
+              <div className="flex-1 shrink gap-2 self-stretch mt-10 w-full text-xl font-semibold leading-tight text-black">
+                Censored
+              </div>
+            </div>
+            <div className="flex flex-col grow shrink justify-between items-center p-3 rounded border border-solid border-neutral-300 min-h-[124px]">
+              <div className="flex gap-2.5 items-center w-full h-9">
+                <img src={Visibility} className="object-contain self-stretch my-auto w-12 aspect-square" alt="" />
+              </div>
+              <div className="flex-1 shrink gap-2 self-stretch mt-10 w-full text-xl font-semibold leading-tight text-black">
+                Not censored
+              </div>
+            </div>
+            <div className="flex flex-col grow shrink justify-between items-center p-3 rounded border border-solid border-neutral-300 min-h-[124px]">
+              <div className="flex gap-2.5 items-center w-full h-9">
+                <img src={LostInTranslation} className="object-contain self-stretch my-auto w-12 aspect-square" alt="" />
+              </div>
+              <div className="flex-1 shrink gap-2 self-stretch mt-10 w-full text-xl font-semibold leading-tight text-black">
+                Lost in translation
+              </div>
+            </div>
           </div>
         </div>
         <div className="flex overflow-hidden justify-center items-start h-full min-w-[240px] w-[395px]">
@@ -29,20 +52,26 @@ function VotingSection() {
             <div className="flex gap-2.5 items-start w-full text-2xl min-h-[61px] text-zinc-400">
               <div className="flex flex-1 shrink gap-2.5 items-center w-full basis-0 min-w-[240px]">
                 <div className="self-stretch my-auto">How is this translation?</div>
-                <img loading="lazy" src="https://cdn.builder.io/api/v1/image/assets/TEMP/aa49eaaae6f0a6491e499c65def80066487dab13b20e659c0087e72f2ad2fbd4?placeholderIfAbsent=true&apiKey=d56f8d62d9074d509de3faeb2651bd99" className="object-contain shrink-0 self-stretch my-auto w-6 aspect-square" alt="" />
+                <img src={Question} className="object-contain shrink-0 self-stretch my-auto w-6 aspect-square" alt="" />
               </div>
             </div>
             <div className="flex gap-4 items-start mt-8 w-full rounded-md">
-              {['Bad translation', 'Good translation'].map((option, index) => (
-                <div key={index} className="flex flex-col justify-between p-3 rounded border border-solid bg-slate-200 border-zinc-400 min-h-[124px] min-w-[190px] w-[190px]">
-                  <div className="flex gap-2.5 items-center w-full h-9">
-                  <img loading="lazy" src={`http://b.io/ext_${20 + index}-`} className="object-contain self-stretch my-auto w-12 aspect-square" alt="" />
+              <div className="flex flex-col justify-between p-3 rounded border border-solid bg-slate-200 border-zinc-400 min-h-[124px]">
+                <div className="flex gap-2.5 items-center w-full h-9">
+                  <img src={ThumbDown} className="object-contain self-stretch my-auto w-12 aspect-square" alt="" />
                 </div>
                 <div className="flex-1 shrink gap-2 self-stretch mt-10 w-full text-xl font-semibold leading-tight text-zinc-400">
-                  {option}
+                  Bad translation
                 </div>
               </div>
-              ))}
+              <div className="flex flex-col justify-between p-3 rounded border border-solid bg-slate-200 border-zinc-400 min-h-[124px]">
+                <div className="flex gap-2.5 items-center w-full h-9">
+                  <img src={ThumbUp} className="object-contain self-stretch my-auto w-12 aspect-square" alt="" />
+                </div>
+                <div className="flex-1 shrink gap-2 self-stretch mt-10 w-full text-xl font-semibold leading-tight text-zinc-400">
+                  Good translation
+                </div>
+              </div>
             </div>
           </div>
         </div>
@@ -55,11 +84,13 @@ function VotingSection() {
           <div className="flex gap-2.5 items-center self-stretch my-auto min-w-[240px] max-md:max-w-full">
             <div className="self-stretch my-auto text-black">See past results for this query in the</div>
             <div className="flex gap-2 items-center self-stretch my-auto text-red-600">
-              <img loading="lazy" src="https://cdn.builder.io/api/v1/image/assets/TEMP/27152eb916f9be9e8c9f7d305bf1ab41ab7ea2c9627a6beceefffa8c8299ed0d?placeholderIfAbsent=true&apiKey=d56f8d62d9074d509de3faeb2651bd99" className="object-contain shrink-0 self-stretch my-auto w-8 aspect-square" alt="" />
-              <div className="self-stretch my-auto underline">
-                <span className="font-bold text-red-600 underline">Archive</span>
-                <span className="text-red-600">.</span>
-              </div>
+              <img src={Archive} className="object-contain shrink-0 self-stretch my-auto w-8 aspect-square" alt="Archive" />
+              <Link to="/archive">
+                <div className="self-stretch my-auto underline">
+                  <span className="font-bold text-red-600">Archive</span>
+                  <span className="text-red-600">.</span>
+                </div>
+              </Link>
             </div>
           </div>
         </div>
