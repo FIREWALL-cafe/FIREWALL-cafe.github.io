5719d5376d3bee8fddb3badf56c913afc1c5777c
Gabriel Williams <ummonai@gmail.com>
2024-11-08 10:25:00 -0800

    about page correctly centered

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -9,34 +9,37 @@ import CallToAction from './CallToAction';
 function About() {
   return (
     <main>
-      <section className="flex overflow-hidden flex-col justify-center py-32 w-full text-6xl leading-tight tracking-[2.16px] max-md:py-24 max-md:max-w-full max-md:text-4xl">
-        <div className="flex flex-col max-w-[1080px] max-md:max-w-full items-center">
-          <div className="chinese font-medium flex flex-col items-center max-w-full">
-            <h2 className="flex flex-col justify-center items-center text-6xl leading-tight max-w-[1080px] tracking-[2.16px] max-md:max-w-full max-md:text-4xl">
-              What do we lose in the dark?
-            </h2>
-            <div className="mt-2 text-7xl leading-tight text-red-600 tracking-[2.16px] max-md:max-w-full max-md:text-4xl">
-              我们在黑暗中失去了什么?
+      <section className="flex overflow-hidden flex-col justify-center w-full bg-white max-md:max-w-full"></section>
+        <div className="flex flex-col items-end pt-12 px-20 max-md:px-5">
+          <div className="flex flex-col w-full max-md:max-w-full">
+            <div className="flex flex-col max-md:max-w-full items-center">
+              <div className="chinese font-medium flex flex-col items-center max-w-full">
+                <h2 className="flex flex-col justify-center items-center text-6xl leading-tight tracking-[2.16px] max-md:max-w-full max-md:text-4xl">
+                  What do we lose in the dark?
+                </h2>
+                <div className="mt-2 text-7xl leading-tight text-red-600 tracking-[2.16px] max-md:max-w-full max-md:text-4xl">
+                  我们在黑暗中失去了什么?
+                </div>
+              </div>
             </div>
-          </div>
+            <div className="flex flex-wrap gap-10 justify-center mt-24 w-full max-w-[1080px] max-md:mt-10 max-md:max-w-full">
+              <div className="flex flex-col flex-1 shrink my-auto basis-0 min-w-[240px] max-md:max-w-full">
+                <p className="text-xl max-md:max-w-full mb-4">
+                  FIREWALL Cafe exists to advocate for freedom of speech for the netizens around the world. Our goal is to investigate online censorship by comparing the disparities of Google searches in western nations versus Baidu searches in China. We aim to increase public awareness and dialogue about Internet freedom. We believe that power, if left unchallenged, inevitably leads to abuse.
+                </p>
+                <p className="text-xl max-md:max-w-full">
+                  A pivotal moment occurred during our inaugural show in New York City in 2016 around our “Networked Feminism” roundtable. We faced interference from Chinese state authorities and this incident was covered by The Washington Post. The repercussions propelled the art project on an international exhibition tour, setting new stages for dialogue and understanding across borders.
+                </p>
+              </div>
+              <div className="flex flex-col justify-center items-center min-w-[240px] w-[516px] max-md:max-w-full">
+                <img loading="lazy" src="https://cdn.builder.io/api/v1/image/assets/TEMP/5c49e04bc86e21ff43a3bd05456f7c220ddd3dbb5d573f89ca6d4a5301b63a96?placeholderIfAbsent=true&apiKey=d56f8d62d9074d509de3faeb2651bd99" alt="Illustration related to the topic" className="object-contain max-w-full aspect-[1.07] w-[395px]" />
+              </div>
+            </div>
+            <Artist />
+            <Supporters />
+          <CallToAction />
         </div>
-        <div className="flex flex-wrap gap-10 justify-center mt-24 w-full max-w-[1080px] max-md:mt-10 max-md:max-w-full">
-          <div className="flex flex-col flex-1 shrink my-auto basis-0 min-w-[240px] max-md:max-w-full">
-            <p className="text-xl max-md:max-w-full mb-4">
-              FIREWALL Cafe exists to advocate for freedom of speech for the netizens around the world. Our goal is to investigate online censorship by comparing the disparities of Google searches in western nations versus Baidu searches in China. We aim to increase public awareness and dialogue about Internet freedom. We believe that power, if left unchallenged, inevitably leads to abuse.
-            </p>
-            <p className="text-xl max-md:max-w-full">
-              A pivotal moment occurred during our inaugural show in New York City in 2016 around our “Networked Feminism” roundtable. We faced interference from Chinese state authorities and this incident was covered by The Washington Post. The repercussions propelled the art project on an international exhibition tour, setting new stages for dialogue and understanding across borders.
-            </p>
-          </div>
-          <div className="flex flex-col justify-center items-center min-w-[240px] w-[516px] max-md:max-w-full">
-            <img loading="lazy" src="https://cdn.builder.io/api/v1/image/assets/TEMP/5c49e04bc86e21ff43a3bd05456f7c220ddd3dbb5d573f89ca6d4a5301b63a96?placeholderIfAbsent=true&apiKey=d56f8d62d9074d509de3faeb2651bd99" alt="Illustration related to the topic" className="object-contain max-w-full aspect-[1.07] w-[395px]" />
-          </div>
-        </div>
-      </section>
-      <Artist />
-      <Supporters />
-      <CallToAction />
+      </div>
     </main>
   );
 }
