067e8b7a8aa8a057228a7f6b67872f105eaf1c8a
Gabriel Williams <ummonai@gmail.com>
2024-11-08 18:22:15 -0800

    press page and article content

--- src/components/FeaturedArticle.jsx
+++ src/components/FeaturedArticle.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import ImagesMode from '../assets/icons/imagesmode.svg';
+import PressPost from '../assets/images/press-washington_post.jpg';
 import ArrowRight from "../assets/icons/arrow_right_alt.svg";
 
 function FeaturedArticle() {
@@ -19,16 +19,16 @@ function FeaturedArticle() {
               </div>
             </div>
             <p className="mt-10 leading-9 text-black max-md:max-w-full">
-              Article preview line - Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus quis viverra mauris. Cras pretium blandit arcu ut accumsan.
+              On the eve of the event, one of the speakers, a visiting Chinese feminist who had done significant work on gender law issues, suddenly started receiving threats.
             </p>
-            <a href="#" className="flex gap-1 justify-center items-center self-start px-4 mt-10 text-xl text-center text-red-600 bg-white rounded border border-red-600 border-solid min-h-[56px]">
+            <a href="https://www.washingtonpost.com/news/worldviews/wp/2016/03/11/how-a-new-york-art-show-about-chinese-online-censorship-found-itself-censored/" target="_blank" className="flex gap-1 justify-center items-center self-start px-4 mt-10 text-xl text-center text-red-600 bg-white rounded border border-red-600 border-solid min-h-[56px]">
               <span className="self-stretch my-auto">Read article</span>
               <img src={ArrowRight} alt="" className="object-contain shrink-0 self-stretch my-auto w-6 aspect-square" />
             </a>
           </div>
         </div>
         <div className="flex flex-col flex-1 shrink justify-center items-center basis-0 min-w-[240px] max-md:max-w-full">
-          <img src={ImagesMode} alt="Featured article illustration" className="object-contain max-w-full aspect-[1.47] w-[432px]" />
+          <img src={PressPost} alt="Featured article illustration" className="object-contain max-w-full aspect-[1.47] w-[432px]" />
         </div>
       </div>
     </section>
