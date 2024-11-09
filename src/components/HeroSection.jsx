1b229ed5abc26f107c109e6881758b9176a0f536
Gabriel Williams <ummonai@gmail.com>
2024-11-08 22:59:25 -0800

    omg typewriter and clean up console logs

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -1,22 +1,27 @@
 import React, { useState, useEffect } from 'react';
 import SearchInput from './SearchInput';
 
-function Typewriter({ text, speed }) {
+function Typewriter({ titles, speed }) {
   const [typedText, setTypedText] = useState('');
   const [currentIndex, setCurrentIndex] = useState(0);
+  const [titleIndex, setTitleIndex] = useState(0);
 
   useEffect(() => {
     const intervalId = setInterval(() => {
+      const text = titles[titleIndex];
       if (currentIndex < text.length) {
         setTypedText(text.slice(0, currentIndex + 1));
         setCurrentIndex((prev) => prev + 1);
+      } else if (currentIndex === text.length && titleIndex < titles.length - 1) {
+        setCurrentIndex(0);
+        setTitleIndex((prev) => (prev + 1));
       } else {
         clearInterval(intervalId);
       }
     }, speed);
 
     return () => clearInterval(intervalId); // Clean up on unmount
-  }, [currentIndex, speed, text]);
+  }, [currentIndex, speed, titles, titleIndex]);
 
   return (
     <div>{typedText}</div>
@@ -36,7 +41,7 @@ function HeroSection() {
   return (
     <section className="flex overflow-hidden flex-col justify-center items-center py-16 w-full bg-white max-md:py-16 max-md:max-w-full">
       <h1 aria-live="polite" className="chinese pt-0 text-6xl font-medium leading-tight text-center text-black border-black tracking-[2.16px] max-md:max-w-full max-md:text-4xl">
-        <Typewriter text={titles[Math.floor(Math.random() * titles.length)]} speed={50} />
+        <Typewriter titles={titles} speed={50} />
       </h1>
       <SearchInput searchMode="live" />
     </section>
