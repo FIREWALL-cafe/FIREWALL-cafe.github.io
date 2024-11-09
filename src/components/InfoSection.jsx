1dfe45f4bb4fa8794b38988f64e8e170e5031ca7
Gabriel Williams <ummonai@gmail.com>
2024-11-09 12:15:02 -0800

    about and homepage

--- src/components/InfoSection.jsx
+++ src/components/InfoSection.jsx
@@ -5,7 +5,7 @@ function InfoSection() {
   return (
     <section className="flex overflow-hidden flex-col justify-center items-center px-36 pb-16 w-full max-md:px-5 max-md:pb-24 max-md:max-w-full">
       <div className="flex flex-col items-center w-full max-w-[1080px] max-md:max-w-full">
-        <div className="chinese flex flex-col justify-center items-start w-full text-5xl font-medium leading-tight max-md:text-4xl">
+        <div className="chinese flex flex-col justify-center items-start items-center w-full text-5xl font-medium leading-tight max-md:text-4xl">
           <h2 className="text-black border-black max-md:max-w-full max-md:text-4xl">
             <Link to="/search">Peer over the wall. Decide for yourself.</Link>
           </h2>
@@ -19,9 +19,14 @@ function InfoSection() {
         <p className="gap-8 self-stretch mt-10 w-full text-2xl leading-9 max-md:max-w-full">
           The goal of this art project is to educate the public about internet freedom and censorship.
         </p>
-        <p className="gap-8 self-stretch mt-10 w-full text-2xl leading-9 max-md:max-w-full">
-          <Link to="/archive" className="underline">What are others seeking over the wall?</Link>
-        </p>
+        <div className="flex gap-1 justify-center items-center px-4 mt-10 text-lg leading-snug text-red-600 bg-white rounded border border-red-600 border-solid min-h-[56px]">
+          <div className="self-stretch my-auto"><Link to="/search">Start Searching</Link></div>
+          <img
+            loading="lazy"
+            src="https://cdn.builder.io/api/v1/image/assets/TEMP/f85ee68f32688d30cdee38a11d3127d6fae6f9fafeb8b4877920d03ca15e202e?placeholderIfAbsent=true&apiKey=d56f8d62d9074d509de3faeb2651bd99"
+            className="object-contain shrink-0 self-stretch my-auto w-6 aspect-square"
+          />
+        </div>
       </div>
     </section>
   );
