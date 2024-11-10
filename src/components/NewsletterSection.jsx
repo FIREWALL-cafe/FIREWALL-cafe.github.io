243b5f4a7ca87e1e7615c99de1e4053fb7c99b01
Gabriel Williams <ummonai@gmail.com>
2024-11-10 11:07:22 -0800

    clean up link contents

--- src/components/NewsletterSection.jsx
+++ src/components/NewsletterSection.jsx
@@ -5,7 +5,7 @@ function NewsletterSection({image}) {
   return (
     <section className="flex overflow-hidden relative justify-center items-start w-full bg-gray-800 border-0 border-black border-solid min-h-[500px] max-md:max-w-full">
       <SubscribeSection />
-      <div className="w-1/2">
+      <div className="w-1/2 border border-b-1 border-black border-solid">
         {image}
       </div>
     </section>
