8637be8ec9755298570f15cbee2359097d9e8299
Gabriel Williams <ummonai@gmail.com>
2024-11-10 15:29:52 -0800

    press content and styles

--- src/components/ArticleCard.jsx
+++ src/components/ArticleCard.jsx
@@ -1,13 +1,20 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-function ArticleCard({ image, title, date, tag, url }) {
+function ArticleCard({ image, imageHover, title, date, tag, url }) {
   return (
     <article className="flex overflow-hidden flex-col grow shrink self-stretch my-auto w-64 rounded border border-black border-solid min-h-[400px] min-w-[240px]">
-      <img src={image} className="object-contain flex-1 w-full mt-4 aspect-[1.78]" />
+      <Link to={url} target="_blank">
+        <img
+          src={image}
+          onMouseOver={e => (e.currentTarget.src = imageHover)}
+          onMouseOut={e => (e.currentTarget.src = image)}
+          className="object-contain flex-1 w-full mt-4 aspect-[1.78]"
+        />
+      </Link>
       <div className="flex flex-col justify-between px-5 py-6 w-full bg-white min-h-[220px]">
         <h3 className="flex-1 text-xl leading-8 text-black text-ellipsis">
-          <Link to={url}>{title}</Link>
+          <Link to={url} target="_blank">{title}</Link>
         </h3>
         <div className="flex gap-3 items-center mt-8 w-full h-5 text-lg text-zinc-400">
           <time className="flex-1 shrink gap-2 self-stretch my-auto w-full min-w-[240px] text-ellipsis">
@@ -17,7 +24,12 @@ function ArticleCard({ image, title, date, tag, url }) {
       </div>
       {tag && (
         <div className={`absolute top-4 right-4 gap-2 self-start px-2 py-1 text-base font-medium ${tag.color || 'text-red-600'} bg-white rounded shadow-sm`}>
-          {tag.icon && <img loading="lazy" src={tag.icon} alt="" className="object-contain shrink-0 self-stretch my-auto w-4 aspect-square" />}
+          {tag.icon &&
+            <img
+              src={tag.icon}
+              alt=""
+              className="object-contain shrink-0 self-stretch my-auto w-4 aspect-square"
+            />}
           <span>{tag.text}</span>
         </div>
       )}
--- src/components/ArticleGrid.jsx
+++ src/components/ArticleGrid.jsx
@@ -1,25 +1,56 @@
 import React from 'react';
 import ArticleCard from './ArticleCard';
-import PressSmart from '../assets/images/press-smart_redirect-1100x696.jpg';
+import PressRussia from '../assets/images/press-russia.jpg.webp';
+import PressHKFP from '../assets/images/press/press-hkfp-default.jpg';
+import PressHKFPHover from '../assets/images/press/press-hkfp-hover.jpg';
+import PressBurnway from '../assets/images/press/Press-Burnway-default.jpg';
+import PressBurnwayHover from '../assets/images/press/Press-Burnway-hover.jpg';
+import PressMountain from '../assets/images/press/press-mountain_xpress-default.jpg';
+import PressMountainHover from '../assets/images/press/press-mountain_xpress-hover.jpg';
+import PressBBC from '../assets/images/press/press-bbc-default.jpg';
+import PressBBCHover from '../assets/images/press/press-bbc-hover.jpg';
+import PressChinaUnscripted from '../assets/images/press/press-china_unscripted-default.jpg';
+import PressChinaUnscriptedHover from '../assets/images/press/press-china_unscripted-hover.jpg';
 
 const articles = [
   {
-    id: 1,
-    image: "/press-smart_redirect-1100x696.jpg",
+    image: PressBurnway,
+    imageHover: PressBurnwayHover,
+    title: "Slow Connection: TSA GVL presents REDIRECT in Asheville",
+    url: "https://burnaway.org/tsa-gvl-redirect-avl/",
+    date: "Feb 13, 2020"
+  },
+  {
+    image: PressMountain,
+    imageHover: PressMountainHover,
     title: "Smart Bets: REDIRECT",
     url: "https://mountainx.com/arts/smart-bets-redirect/",
     date: "Jan 20, 2020"
   },
   {
-    id: 3,
-    image: "/images/press-russia.jpg.webp",
+    image: PressBBC,
+    imageHover: PressBBCHover,
     title: "The Real Story: Russia's New Internet Firewall (cue 35:06)",
     url: "https://www.bbc.co.uk/sounds/play/w3csydds",
     date: "Nov 1, 2019",
   },
   {
-    id: 5,
-    image: "/images/press-hkfp.jpg",
+    image: PressChinaUnscripted,
+    imageHover: PressChinaUnscriptedHover,
+    title: "#42 Hacking China’s Censorship | FIREWALL Cafe",
+    url: "http://chinaunscripted.libsyn.com/42-hacking-chinas-censorship-firewall-cafe",
+    date: "Aug 23, 2019",
+  },
+  {
+    image: PressHKFP,
+    imageHover: PressHKFPHover,
+    title: "Two artists ask: what does it mean to live under the shadow of the Great Firewall",
+    url: "https://www.hongkongfp.com/2019/01/20/two-artists-ask-what-does-it-mean-to-live-under-the-shadow-of-the-great-firewall/",
+    date: "January 20, 2019"
+  },
+  {
+    image: PressHKFP,
+    imageHover: PressHKFPHover,
     title: "Google vs. Baidu – Artist Joyce Yu-Jean Lee examines cultural and political bias online… and in ourselves",
     url: "https://www.hongkongfp.com/2019/02/24/video-google-vs-baidu-artist-joyce-yu-jean-lee-examines-cultural-political-bias-online/",
     date: "February 24, 2019"
