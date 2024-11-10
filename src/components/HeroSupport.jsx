5bce73e71c8c61597d057ec8fcd79413370b9ac6
Gabriel Williams <ummonai@gmail.com>
2024-11-09 21:33:15 -0800

    newsletter and contact form and social follow

--- src/components/HeroSupport.jsx
+++ src/components/HeroSupport.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
+import SupportHero from '../assets/images/support-hero.png';
 
 function HeroSupport() {
   return (
-    <section className="flex overflow-hidden flex-col items-center py-32 w-full bg-white max-md:py-24 max-md:max-w-full">
+    <section className="flex overflow-hidden flex-col items-center py-16 w-full bg-white max-md:py-24 max-md:max-w-full">
       <div className="chinese flex flex-col items-center max-w-full min-w-[388px]">
         <h1 className="flex flex-col justify-center items-center self-stretch w-full text-5xl font-medium text-center max-md:max-w-full max-md:text-4xl">
           Support the frontline of internet freedom advocates
@@ -11,24 +12,16 @@ function HeroSupport() {
           支持互联网自由战士的前线。
         </div>
       </div>
-      <div className="flex flex-wrap gap-10 justify-center mt-32 max-w-full w-[1080px] max-md:mt-10">
-        <div className="flex flex-col flex-1 shrink my-auto text-2xl leading-9 text-black basis-0 min-w-[240px] max-md:max-w-full">
+      <div className="flex gap-10 mt-24 max-w-full w-[1080px] max-md:mt-10">
+        <div className="flex flex-col flex-1 shrink text-2xl leading-9 text-black basis-0 min-w-[240px] max-md:max-w-full">
           <p className="w-full max-md:max-w-full">
             FIREWALL Cafe is sustained through individual donations, corporate sponsorships, and foundation grants. Your contributions help maintain our dual-image browser and enable us to continue our growth internationally.</p>
-          <p>Donate to our cause through <a href="https://www.nyfa.org/#button=45138" className="underline" target="_blank">NYFA</a>, a 501(c)3 supporting the arts in NY.</p>
+          <p className="mt-5">Donate to our cause through <a href="https://www.nyfa.org/#button=45138" className="underline" target="_blank">NYFA</a>, a 501(c)3 supporting the arts in NY.</p>
         </div>
-        <div className="flex flex-col flex-1 shrink justify-center items-center basis-0 min-w-[240px] max-md:max-w-full">
-          <img loading="lazy" src="https://cdn.builder.io/api/v1/image/assets/TEMP/db738558fc4a68decfbd7caa2ee7cceba38b2cefd3af6c910907a86363f4cb6c?placeholderIfAbsent=true&apiKey=d56f8d62d9074d509de3faeb2651bd99" className="object-contain max-w-full aspect-[1.47] w-[432px]" alt="Donation illustration" />
+        <div className="flex flex-col flex-1 shrink justify-center items-center basis-0 w-1/2 max-md:max-w-full">
+          <img src={SupportHero} className="object-contain max-w-full aspect-[1.47]" alt="Donation illustration" />
         </div>
       </div>
-      {/* <div className="flex flex-col items-start mt-32 max-w-full text-4xl font-medium leading-10 text-center w-[720px] max-md:mt-10">
-        <p className="text-black max-md:max-w-full">
-          With your support we pledge to XYZ and carry out our mission of 123.
-        </p>
-        <p className="chinese text-red-600 max-md:max-w-full">
-          在您的支持下，我们向 XYZ 承诺并履行我们的 123 使命。
-        </p>
-      </div> */}
     </section>
   );
 }
--- src/components/NewsletterSection.jsx
+++ src/components/NewsletterSection.jsx
@@ -1,28 +1,11 @@
 import React from "react";
 import UsHeadlines from "../assets/images/subscribe-d-desktop.jpg";
-import SubscribeMailchimp from "../assets/images/subscribe_mailchimp.jpg";
+import SubscribeSection from "./SubscribeSection";
 
 function NewsletterSection() {
   return (
     <section className="flex overflow-hidden relative justify-center items-start w-full bg-gray-800 border-0 border-black border-solid min-h-[500px] max-md:max-w-full">
-      <div className="flex flex-col px-24 justify-center items-start self-stretch w-1/2 max-md:max-w-full">
-        <div className="flex flex-col max-w-full border-0 border-white border-solid shadow-sm">
-          <div className="flex flex-col w-full shadow-sm">
-            <div className="chinese flex flex-col w-full text-5xl font-medium leading-tight max-md:text-4xl">
-              <h2 className="text-white max-md:text-4xl">Subscribe</h2>
-              <div className="text-red-600 max-md:text-4xl">订阅</div>
-            </div>
-            <div className="flex flex-col mt-5 w-full text-xl">
-              <p className="leading-8 text-white w-3/4">
-                Get newsletter updates about upcoming events, press releases, and expert editorial.
-              </p>
-              <div className="w-1/2">
-                <img src={SubscribeMailchimp} />
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
+      <SubscribeSection />
       <div className="w-1/2">
         <img src={UsHeadlines} />
       </div>
