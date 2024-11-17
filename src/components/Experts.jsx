3abc8a0d470802d4e23df83abdb082aac3e39600
Gabriel Williams <ummonai@gmail.com>
2024-11-17 11:41:54 -0800

    editorial consistent at mobile and desktop

--- src/components/Experts.jsx
+++ src/components/Experts.jsx
@@ -10,8 +10,8 @@ function Experts() {
 
   return (
     <main className="flex overflow-hidden flex-col bg-white min-h-[200px]">
-      <section className="flex flex-col justify-center items-center px-14 py-16 w-full max-md:px-5 max-md:py-24 max-md:max-w-full">
-        <div className="flex flex-col items-center max-w-full w-[1080px]">
+      <section className="flex flex-col justify-center items-center px-32 py-16 w-full max-md:px-5 max-md:py-24 max-md:max-w-full">
+        <div className="flex flex-col items-center max-w-full">
           <div className="chinese flex flex-col max-w-full text-6xl font-medium text-center leading-tight tracking-[2.16px] w-[664px] max-md:text-4xl">
             <div className="flex flex-wrap gap-5 items-center self-center text-black max-md:max-w-full max-md:text-4xl">
               <img src={Commentary} alt="" className="object-contain shrink-0 self-stretch my-auto aspect-square w-[52px]" />
