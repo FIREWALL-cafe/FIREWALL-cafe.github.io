cf9172a3255b0f8665f67cca290be6a878e73461
Gabriel Williams <ummonai@gmail.com>
2024-11-23 13:47:00 -0800

    WIP

--- src/components/QueryItem.jsx
+++ src/components/QueryItem.jsx
@@ -1,9 +1,12 @@
 import React, { useState } from 'react';
 import SearchCompare from './SearchCompare';
-
-const QueryItem = ({ id, tags, title, excerpt, date, galleries, searchId }) => {
+import ThumbUp from '../assets/icons/thumb_up.svg';
+const QueryItem = ({ search_id, search_term_initial, search_term_translation, search_location, search_timestamp, image_hrefs }) => {
   const capitalize = str => `${str[0].toUpperCase()}${str.slice(1)}`;
+  const humanize = str => str.split('_').map(capitalize).join(' ');
+  const formatDate = timestamp => new Date(parseInt(timestamp)).toLocaleDateString();
 
+  const [status, setStatus] = useState("loading");
   const [dropdown, setDropdown] = useState(true);
   const toggleDropdown = () => setDropdown(!dropdown);
 
@@ -25,24 +28,26 @@ const QueryItem = ({ id, tags, title, excerpt, date, galleries, searchId }) => {
   };
 
   return (
-    <div className="flex flex-wrap gap-4 py-2 w-full min-h-[48px] max-md:max-w-full hover:bg-sky-100">
-      <div className="flex flex-wrap gap-4 py-2 w-full text-xl text-black min-h-[48px] max-md:max-w-full" onClick={toggleDropdown}>
-        <div className="flex flex-1 shrink gap-1 items-center h-full basis-0 min-w-[240px]">
-          <div className="z-10 flex-1 shrink self-stretch my-auto w-full min-w-[240px]">{title.rendered}</div>
-        </div>
-        <div className="flex flex-1 shrink gap-1 items-center h-full basis-0 min-w-[240px] text-zinc-400">
-          <div className="z-10 flex-1 shrink self-stretch my-auto w-full min-w-[240px]">{excerpt.rendered.replace(/<[^>]*>/g, '')}</div>
-        </div>
-        <div className="flex flex-1 shrink gap-1 items-center h-full basis-0 min-w-[240px]">
-          <div className="z-10 flex-1 shrink self-stretch my-auto w-full min-w-[240px]">{parseLocation(tags)}</div>
-        </div>
-        <div className="flex gap-6 items-center h-full whitespace-nowrap w-[191px]">
-          <div className="flex-1 shrink self-stretch my-auto basis-0">{new Date(date).toLocaleDateString()}</div>
-        </div>
+    <div className="flex flex-wrap gap-4 py-2 w-full text-xl text-black min-h-[48px] max-md:max-w-full">
+      <div className="flex gap-1 items-center my-auto w-16 whitespace-nowrap">
+        <img loading="lazy" src={ThumbUp} alt="" className="object-contain shrink-0 self-stretch my-auto w-6 aspect-square" />
+        <div className="self-stretch my-auto">???</div>
       </div>
-      <div className={`flex flex-wrap`} style={dropdown ? { display: 'none' } : {}}>
-        {displayCarousel(galleries[0].src, galleries[1].src) ? <SearchCompare images={ { googleResults: galleries[0].src.slice(0, 9), baiduResults: galleries[1].src.slice(0, 9) } } searchId={id} /> : <div className="border border-dashed border-red-600 m-5 px-5">No images to display</div>}
+      <div className="flex flex-1 shrink gap-1 items-center h-full basis-0 min-w-[240px]">
+        <div className="z-10 flex-1 shrink self-stretch my-auto w-full min-w-[240px]">{search_term_initial}</div>
       </div>
+      <div className="flex flex-1 shrink gap-1 items-center h-full basis-0 min-w-[240px] text-zinc-400">
+        <div className="z-10 flex-1 shrink self-stretch my-auto w-full min-w-[240px]">{search_term_translation}</div>
+      </div>
+      <div className="flex flex-1 shrink gap-1 items-center h-full basis-0 min-w-[240px]">
+        <div className="z-10 flex-1 shrink self-stretch my-auto w-full min-w-[240px]">{search_location && humanize(search_location)}</div>
+      </div>
+      <div className="flex gap-6 items-center h-full whitespace-nowrap w-[191px]">
+        <div className="flex-1 shrink self-stretch my-auto basis-0">{formatDate(search_timestamp)}</div>
+        </div>
+        <div className="flex flex-1">
+          <img src={image_hrefs[0]} onLoad={handleOnLoad} onError={handleOnError} className={`object-contain shrink-0 my-auto w-8 aspect-square ${status === "fulfilled" ? "visible" : "hidden"}`} />
+        </div>
     </div>
   );
 };
--- src/components/QueryList.jsx
+++ src/components/QueryList.jsx
@@ -4,18 +4,18 @@ import QueryItem from './QueryItem';
 const QueryList = ({ results }) => {
   return (
     <section className="flex overflow-hidden flex-col pb-16 w-full bg-white max-md:pb-24 max-md:max-w-full">
-      
-      <div className="flex-1 shrink gap-2.5 self-stretch px-10 pt-6 w-full max-w-screen-xl text-2xl text-black min-h-[57px] max-md:px-5 max-md:max-w-full">
+      <div className="flex-1 shrink gap-2.5 self-stretch px-20 pb-6 w-full max-w-screen-xl text-2xl text-black min-h-[57px] max-md:px-5 max-md:max-w-full">
         {results.length} related queries
       </div>
       <div className="flex flex-col items-center px-9 w-full max-md:px-5 max-md:max-w-full">
         <div className="flex flex-col w-full max-w-screen-xl max-md:max-w-full">
           <div className="flex flex-wrap gap-4 py-2 w-full text-xl text-black border-b border-solid border-b-neutral-300 min-h-[48px] max-md:max-w-full">
+            <div className="gap-1 self-stretch my-auto w-16 whitespace-nowrap">Votes</div>
             <div className="flex flex-1 shrink gap-1 items-center h-full basis-0">
-              <div className="z-10 flex-1 shrink self-stretch my-auto w-full">Query</div>
+              <div className="z-10 flex-1 shrink self-stretch my-auto w-full">Query EN</div>
             </div>
             <div className="flex flex-1 shrink gap-1 items-center h-full basis-0">
-              <div className="z-10 flex-1 shrink self-stretch my-auto w-full">Translation</div>
+              <div className="z-10 flex-1 shrink self-stretch my-auto w-full">搜索结果 中文</div>
             </div>
             <div className="flex flex-1 shrink gap-1 items-center h-full whitespace-nowrap basis-0">
               <div className="z-10 flex-1 shrink self-stretch my-auto w-full">Location</div>
@@ -31,6 +31,10 @@ const QueryList = ({ results }) => {
           <div className="flex flex-col w-full min-h-[24px] max-md:max-w-full">
             <div className="w-full border border-solid bg-zinc-400 border-zinc-400 min-h-[1px] max-md:max-w-full" />
           </div>
+          {/* <div className="flex flex-col items-center self-center px-6 py-7 max-w-full text-2xl text-black w-[161px] max-md:px-5">
+            <div>load more</div>
+            <img loading="lazy" src="https://cdn.builder.io/api/v1/image/assets/TEMP/83c361a622b5a07e3d4834157a197144fe9143c6fdd77a9f991a2906a2b02001?placeholderIfAbsent=true&apiKey=d56f8d62d9074d509de3faeb2651bd99" alt="" className="object-contain mt-3 w-9 aspect-[4]" />
+          </div> */}
         </div>
       </div>
     </section>
