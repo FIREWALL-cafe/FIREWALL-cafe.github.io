43e1fec14d39581cb4627dd199f50a24e20b7376
Gabriel Williams <ummonai@gmail.com>
2025-03-04 01:47:02 -0800

    remove etags

--- index.js
+++ index.js
@@ -15,18 +15,18 @@ app.use((err, req, res, next) => {
   res.status(500).send('Something broke!')
 })
 
-app.use(express.static(path.join(__dirname, "build")));
+app.use(express.static(path.join(__dirname, "build"), { etag: false, lastModified: false }));
 
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "build", "index.html"));
+  res.sendFile(path.join(__dirname, "build", "index.html"), { etag: false, lastModified: false });
 });
 
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "build", "index.html"));
+  res.sendFile(path.join(__dirname, "build", "index.html"), { etag: false, lastModified: false });
 });
 
 app.get("/*", (req, res) => {
-  res.sendFile(path.join(__dirname, "build", "index.html"));
+  res.sendFile(path.join(__dirname, "build", "index.html"), { etag: false, lastModified: false });
 });
 
 app.post("/searches/:search_id/images", async (req, res) => {
