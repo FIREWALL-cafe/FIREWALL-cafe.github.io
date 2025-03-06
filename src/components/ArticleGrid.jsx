4b6c1097e96d8fceade0e63c291e7bf411623761
Gabriel Williams <ummonai@gmail.com>
2025-03-05 16:26:41 -0800

    fix press article links

--- src/components/ArticleGrid.jsx
+++ src/components/ArticleGrid.jsx
@@ -58,13 +58,13 @@ const articles = [
   },
   {
     title: "Data Justice: Joyce Yu-Jean Lee on \"The Great Firewall\"",
-    url: "http://www.mediasanctuary.org/",
+    url: "https://www.mediasanctuary.org/podcasts/data-justice-joyce-yu-jean-lee-on-the-great-firewall/",
     date: "March 13, 2018",
     source: "Sanctuary for Independent Media, WOOC 105.3 FM radio"
   },
   {
     title: "Professor creates political commentary through art",
-    url: "https://signalscv.com/",
+    url: "http://www.tcnjsignal.net/2018/02/20/professor-creates-political-commentary-through-art/",
     date: "February 20, 2018",
     source: "The Signal"
   },
@@ -77,48 +77,94 @@ const articles = [
   },
   {
     title: "Art Attack: Ai Weiwei and other artists have increased the popularity of Chinese art, but censorship has followed",
-    url: "https://www.indexoncensorship.org/",
+    url: "https://journals.sagepub.com/doi/full/10.1177/0306422016670329",
     date: "September 20, 2016",
     source: "Index on Censorship"
   },
   {
     title: "How a New York art show about Chinese online censorship found itself censored",
-    url: "https://www.washingtonpost.com/",
+    url: "https://www.washingtonpost.com/news/worldviews/wp/2016/03/11/how-a-new-york-art-show-about-chinese-online-censorship-found-itself-censored/",
     date: "March 11, 2016",
     source: "The Washington Post"
   },
   {
     title: "Surf China's Censored Web at an Internet Cafe in New York",
-    url: "https://hyperallergic.com/",
+    url: "http://hyperallergic.com/280849/surf-chinas-censored-web-at-an-internet-cafe-in-new-york/",
     date: "March 4, 2016",
     source: "Hyperallergic"
   },
   {
     title: "How an Art Exhibition in New York Led to Harassment from Chinese Authorities",
-    url: "https://artfcity.com/",
+    url: "http://artfcity.com/2016/02/26/how-an-art-exhibition-in-new-york-led-to-harassment-from-chinese-authorities/",
     date: "Feb 26, 2016",
     source: "Art F City"
+  },
+  {
+    title: "When Chinese state censorship reached the L.E.S.",
+    url: "http://thevillager.com/2016/03/31/when-chinese-state-censorship-reached-the-l-e-s/",
+    date: "March 31, 2016",
+    source: "The Villager"
+  },
+  // {
+  //   title: "Exercycles and Sweethearts: Firewall Internet Café",
+  //   url: "http://www.artcritical.com/2016/03/03/david-brody-on-firewall-internat-cafe/",
+  //   date: "March 3, 2016",
+  //   source: "Art Critical"
+  // },
+  {
+    title: "華埠「防火牆」網吧 體驗中共網路封鎖",
+    url: "http://chinadigitaltimes.net/2016/03/chinas-overseas-critics-pressure/",
+    date: "March 11, 2016",
+    source: "China's Digital Times",
+    language: "Chinese"
+  },
+  {
+    title: "How China Censors Mass Media in Your World",
+    url: "http://www.eweek.com/cloud/how-china-censors-mass-media-in-your-world.html",
+    date: "March 11, 2016",
+    source: "eWeek",
+  },
+  {
+    title: "You can Surf China’s Censored Web at this Lower East Side Internet Cafe",
+    url: "http://www.papermag.com/manhattan-downtown-china-censored-web-1646138164.html",
+    date: "March 6, 2016",
+    source: "Paper Magazine",
+    language: "English"
   },
   // Chinese Language Press
   {
     title: "華埠「防火牆」網吧 體驗中共網路封鎖",
-    url: "https://www.ntdtv.com/",
+    url: "http://www.ntdtv.com/xtr/b5/2016/03/08/a1256569.html",
     date: "March 7, 2016",
     source: "New Tang Dynasty (NTD) Television",
     language: "Chinese"
   },
   {
     title: "專訪李玉瑾：華裔藝術家為什麼要在紐約開防火牆網吧",
-    url: "https://theinitium.com/",
+    url: "https://theinitium.com/article/20160322-culture-feature-FirewallCafe/",
     date: "March 22, 2016",
     source: "The Initium",
     language: "Chinese"
   },
   {
     title: "網路長城有多威？台裔女孩開網咖讓老美體驗",
-    url: "https://www.appledaily.com.tw/",
+    url: "http://www.appledaily.com.tw/realtimenews/article/international/20160304/808253/applesearch/網路長城有多威？台裔女孩開網咖讓老美體驗",
     date: "March 4, 2016",
     source: "Apple Daily Taiwan",
+    language: "Chinese"
+  },
+  {
+    title: "紐約一特色網咖 體驗內地網絡審查",
+    url: "http://litenewshk.blogspot.com/2016/03/blog-post_88.html",
+    date: "March 3, 2016",
+    source: "Lite News(Hong Kong)",
+    language: "Chinese"
+  },
+  {
+    title: "纽约开了一家体验中美信息审查的网络咖啡馆",
+    url: "https://botanwang.com/articles/201603/纽约开了一家体验中美信息审查的网络咖啡馆.html",
+    date: "March 4, 2016",
+    source: "Botanwang.com",
     language: "Chinese"
   }
 ];
@@ -128,8 +174,8 @@ function ArticleGrid() {
     <section className="flex flex-col items-center px-14 pt-16 pb-16 w-full bg-gray-50 border-t border-solid border-t-neutral-300 max-md:px-5 max-md:pb-24 max-md:max-w-full">
       <div className="flex flex-col w-full max-w-[1080px] max-md:max-w-full">
         <div className="flex gap-8 mb-12">
-          <button className="text-lg font-medium text-gray-700 hover:text-black">English</button>
-          <button className="text-lg font-medium text-gray-700 hover:text-black">中文</button>
+          <a href="#English" className="text-lg font-medium text-gray-700 hover:text-black">English</a>
+          <a href="#Chinese" className="text-lg font-medium text-gray-700 hover:text-black">中文</a>
         </div>
         <div className="flex flex-wrap gap-5 justify-center items-center mt-16 w-full max-md:mt-10 max-md:max-w-full">
           {articles.map((article, index) => (
