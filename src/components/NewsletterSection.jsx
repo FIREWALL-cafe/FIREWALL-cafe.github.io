3338eaa296ce0e40af8b2d44e008421859f2bb32
Gabriel Williams <ummonai@gmail.com>
2025-03-20 15:09:13 -0700

    Trello-85 subscribe image wraps on top

--- src/components/NewsletterSection.jsx
+++ src/components/NewsletterSection.jsx
@@ -4,12 +4,16 @@ import SubscribeSection from "./SubscribeSection";
 function NewsletterSection({image}) {
   return (
     <section className="flex overflow-hidden relative flex-col md:flex-row justify-center items-start w-full 
-      bg-black border-0 border-black border-solid md:max-h-[400px] max-md:max-w-full">
-      <SubscribeSection />
-      <div className="w-full md:w-1/2 min-h-[300px] md:min-h-0 border border-b-1 border-black border-solid">
+      bg-black border-0 border-black border-solid max-md:max-w-full">
+      <div className="w-full md:w-1/2 min-h-[300px] md:min-h-0 border border-b-1 border-black border-solid h-full order-1 md:order-2 p-4 md:p-8 lg:p-12">
         <div className="h-full w-full">
-          {image}
+          {React.cloneElement(image, {
+            className: `w-full h-full object-cover ${image.props.className || ''}`
+          })}
         </div>
+      </div>
+      <div className="order-2 md:order-1 w-full md:w-1/2">
+        <SubscribeSection />
       </div>
     </section>
   );
