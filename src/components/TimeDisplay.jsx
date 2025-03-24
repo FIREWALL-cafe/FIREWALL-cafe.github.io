b48f116ec37968ea566d2a2cf7bcefa4dd53440a
Gabriel Williams <ummonai@gmail.com>
2025-03-23 21:36:07 -0700

    responsive display of time

--- src/components/TimeDisplay.jsx
+++ src/components/TimeDisplay.jsx
@@ -14,7 +14,7 @@ function TimeDisplay() {
     <div className="flex gap-5 items-center h-full font-medium text-center min-w-[240px] text-slate-100">
       <div className="flex gap-5 items-center self-stretch my-auto min-w-[240px]">
         <div className="self-stretch my-auto">Your time: {currentTime}</div>
-        <div className="self-stretch my-auto">Beijing: {timeInShanghai()}</div>
+        <div className="self-stretch my-auto max-md:min-w-0 max-md:hidden">Beijing: {timeInShanghai()}</div>
       </div>
     </div>
   );
