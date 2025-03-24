a9531f2147a4940b6e2cf3bb8c87689943194ca6
Gabriel Williams <ummonai@gmail.com>
2025-03-23 21:58:36 -0700

    Trello-92 featurecard icon hover and size

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -3,8 +3,10 @@ import React from 'react';
 import SearchInput from './SearchInput';
 import FeatureCards from './FeatureCards';
 import Archive from '../assets/icons/Archive_grayscale.png';
+import ArchiveHover from '../assets/icons/Archive.png';
 import SearchIcon from '../assets/icons/search-color.png';
 import Commentary from '../assets/icons/expert-commentary_grayscale.png';
+import CommentaryHover from '../assets/icons/expert-commentary.png';
 
 const features = [
     {
@@ -13,6 +15,7 @@ const features = [
       chineseTitle: { text: "档案", color: "text-black border-black" },
       description: "Explore what other users have searched and vote on results.",
       iconSrc: Archive,
+      iconSrcHover: ArchiveHover,
       bgColor: "bg-red-600",
       textColor: "text-white",
       borderColor: "border-red-600"
@@ -23,6 +26,7 @@ const features = [
       chineseTitle: { text: "专家点评", color: "text-red-600 border-red-600" },
       description: "Read and listen to in-depth commentary from experts.",
       iconSrc: Commentary,
+      iconSrcHover: CommentaryHover,
       bgColor: "bg-white",
       textColor: "text-black",
       borderColor: "border-red-600"
