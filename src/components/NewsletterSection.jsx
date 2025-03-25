95a438a1f9d21d25a70a3d1eaa879da132094cd4
Gabriel Williams <ummonai@gmail.com>
2025-03-25 16:24:14 -0700

    better image handles in subscribe section

--- src/components/NewsletterSection.jsx
+++ src/components/NewsletterSection.jsx
@@ -1,15 +1,22 @@
 import React from "react";
 import SubscribeSection from "./SubscribeSection";
 
+import UsHeadlines from "../assets/images/subscribe-d-desktop.jpg";
+import UsHeadlinesMobile from "../assets/images/subscribe-d-mobile.jpg";
+
 function NewsletterSection({image}) {
   return (
     <section className="flex overflow-hidden relative flex-col md:flex-row justify-center items-start w-full 
       bg-black border-0 border-black border-solid max-md:max-w-full">
-      <div className="max-w-[1080px] mx-auto w-full flex flex-col md:flex-row">
-        <div className="h-full w-full order-1 md:order-2">
-          {React.cloneElement(image, {
-            className: `w-full h-full object-cover ${image.props.className || ''}`
-          })}
+      <div className="mx-auto w-full flex flex-col md:flex-row">
+        <div className="h-full md:w-full w-1/2order-1 md:order-2">
+          <picture>
+            <source media="(max-width: 1079x)" srcset={UsHeadlinesMobile} />
+            <source media="(min-width: 1080px)" srcset={UsHeadlines} />
+            {React.cloneElement(image, {
+              className: `w-full h-full object-cover`
+            })}
+          </picture>
         </div>
         <div className="order-2 md:order-1 w-full md:w-1/2">
           <SubscribeSection />
