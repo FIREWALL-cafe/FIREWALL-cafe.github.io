12c929681f6fe900f154f5f1a20896b7d308edff
Gabriel Williams <ummonai@gmail.com>
2025-03-28 14:03:03 -0700

    start updating typewriter and h1 content

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -7,10 +7,11 @@ import NewsletterSection from "./NewsletterSection";
 
 import UsHeadlines from "../assets/images/subscribe-d-desktop.jpg";
 import UsHeadlinesMobile from "../assets/images/subscribe-d-mobile.jpg";
+
 function Home() {
   const image = <img src={UsHeadlines} alt="US Headlines" className="w-full h-full object-cover" />
   return (
-    <main className="w-full">
+    <main className="w-full bg-neutral-50">
       <div className="max-w-[1080px] mx-auto px-4 md:px-6">
         <HeroSection />
         <AboutSection />
