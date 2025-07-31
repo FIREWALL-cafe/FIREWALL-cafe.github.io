cbd655d6bb73992671d798cff355906519b8cfbe
Gabriel Williams <ummonai@gmail.com>
2025-07-31 16:50:40 -0700

    search module should use 1280px when possible TRELLO-164

--- src/components/SearchCompare.jsx
+++ src/components/SearchCompare.jsx
@@ -8,7 +8,7 @@ const displayVoting = serverConfig.displayVoting;
 function SearchCompare({ images, query, searchId }) {
   return (
     <section id={`search-compare-${searchId}`} className="flex mt-10 gap-2.5 justify-center items-center pb-8 w-full bg-white max-md:max-w-full">
-      <div className="flex overflow-hidden flex-col self-stretch my-auto max-w-screen-xl rounded-lg border border-red-600 border-solid min-w-[240px] w-[1248px] max-md:max-w-full">
+      <div className="flex overflow-hidden flex-col self-stretch my-auto max-w-[1280px] rounded-lg border border-red-600 border-solid min-w-[240px] w-full max-md:max-w-full">
         <ImageCarousel images={images} searchId={searchId} />
         {displayVoting && <VotingSection query={query} searchId={searchId} />}
       </div>
